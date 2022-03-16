6d7054db9ebed486bce005d465be449093c35087
SArpnt <36542932+sarpnt@users.noreply.github.com>
2022-03-16 13:22:29 -0400

    help messages
    Signed-off-by: SArpnt <36542932+sarpnt@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,31 @@ mod parse_args;
 mod parse_flags;
 mod execute;
 
+use operation::{Operation, NonHelpOp};
+
 fn main() -> Result<(), String> { // TODO: change error message formatting to just output directly
 	let base_op = parse_args::parse(std::env::args())?;
-	let complex_op = parse_flags::parse(base_op)?;
+	match base_op.operation {
+		Operation::Help(op) => {
+			print!(
+				"{}",
+				match op { // TODO: help messages
+					None => "",
+					Some(NonHelpOp::Version) => "",
+					Some(NonHelpOp::Database) => "",
+					Some(NonHelpOp::Files) => "",
+					Some(NonHelpOp::Query) => "",
+					Some(NonHelpOp::Remove) => "",
+					Some(NonHelpOp::Sync) => "",
+					Some(NonHelpOp::Deptest) => "",
+					Some(NonHelpOp::Upgrade) => "",
+				}
+			)
+		},
+		Operation::Not(op) => {
+			let complex_op = parse_flags::parse(op, base_op.short_flags, base_op.long_flags, base_op.targets)?;
+		},
+	}
 
 	Ok(())
 }
--- src/parse_flags.rs
+++ src/parse_flags.rs
@@ -1,4 +1,4 @@
-use crate::operation::Operation;
+use crate::operation::NonHelpOp;
 use crate::parse_args::BaseOperation;
 
 pub struct ComplexOperation {
@@ -7,14 +7,14 @@ pub struct ComplexOperation {
 	pub targets: Vec<String>,
 }
 
-pub fn parse(base_op: BaseOperation) -> Result<ComplexOperation, String> {
-	match base_op.operation {
+pub fn parse(operation: NonHelpOp, short_flags: Vec<char>, long_flags: Vec<String>, targets: Vec<String>) -> Result<ComplexOperation, String> {
+	match operation {
 		_ => {return Err(String::from("TODO"));} // TODO
 	}
 	
 	Ok(ComplexOperation {
-		operation: base_op.operation,
+		operation,
 
-		targets: base_op.targets,
+		targets,
 	})
 }
